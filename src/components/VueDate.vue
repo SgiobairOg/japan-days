85feb153f31730f8506dcd6c4947683c7db12982
Jason Wilson <jason.wilson@valtech.com>
2022-03-18 23:33:45 +0100

    feat(furigana): Add Furigana and more Kanji

--- src/components/VueDate.vue
+++ src/components/VueDate.vue
@@ -1,10 +1,8 @@
 <template>
   <section id="vue" class="date-component">
     <header class="date-component__header">
-      <div class="date-component__question">
-        {{ Subject }}は何月何日ですか？
-      </div>
-      <h1 class="date-component__date">{{ Subject }}は{{ Month }}{{ daysDate }}{{ Day }}です。</h1>
+      <div class="date-component__question" v-html="QuestionString"></div>
+      <h1 class="date-component__date" v-html="DateString"></h1>
     </header>
     <div role="button" :class="prevDayClass" @click="previousSubjectDay" @keydown="prevAndNextKeys">Previous Day</div>
     <div role="button" :class="nextDayClass" @click="nextSubjectDay" @keydown="prevAndNextKeys">Next Day</div>
@@ -20,6 +18,10 @@
     grid-row-gap: 1rem;
     height: 100%;
     width: 100%;
+  }
+  .date-component__header rt {
+    opacity: 60%;
+    font-size: 45%;
   }
   .date-component__header {
     display: flex;
@@ -150,32 +152,103 @@ export default {
     const subjects = {
       'min': -3,
       'max': 4,
-      '-3': 'さきおととい',
-      '-2': 'おととい',
-      '-1': 'きのう',
-      '0': 'きよう',
-      '1': 'あした',
-      '2': 'あさって',
-      '3': 'しあさって',
-      '4': 'ししあさって',
+      '-3': '<ruby>一昨昨日<rt>さきおととい</rt></ruby>',
+      '-2': '<ruby>一昨日<rt>おととい</rt></ruby>',
+      '-1': '<ruby>昨日<rt>きのう</rt></ruby>',
+      '0': '<ruby>今日<rt>きょう</rt></ruby>',
+      '1': '<ruby>明日<rt>あした</rt></ruby>',
+      '2': '<ruby>明後日<rt>あさって</rt></ruby>',
+      '3': '<ruby>明明後日<rt>しあさって</rt></ruby>',
+      '4': '<ruby>し明明後日<rt>ししあさって</rt></ruby>',
     }
 
-    const Subject = computed(() => {
+    const DateString = computed(() => {
+      return `${ Subject() }は${ Month() }<span>${ daysDate() }</span>${ Day() }です。`;
+    });
+
+    const QuestionString = computed(() => {
+      return `${ Subject() }は<ruby>何<rt>なん</rt></ruby><ruby>月<rt>がつ</rt></ruby>何<ruby>日<rt>にち</rt></ruby>ですか？`;
+    });
+
+    const Subject = () => {
       return subjects[subjectDay.value];
-    });
+    };
 
-    const Month = computed(() => {
-      return ['１月','２月','３月','４月','５月','６月','７月','８月','９月','１０月','１１月','１２月'][date.value.getMonth()];
-    });
-
-    const Day = computed(() => {
-      return `${['日','月','火','水','木','金','土'][date.value.getDay()]}ようび`;
-    });
-
-    const daysDate = computed(() => {
-       return `${date.value.getDate()}日`
+    const Month = () => {
+      return `${date.value.getMonth()}<ruby>月<rt>がつ</rt></ruby>`
         .replace(/[0-9]{1}/g, shiftCharacter(0xFEE0));
-    });
+    };
+
+    const Day = () => {
+      return `${['日','月','火','水','木','金','土'][date.value.getDay()]}<ruby>曜日<rt>ようび</rt></ruby>`;
+    };
+
+    const daysDate = () => {
+      const dateReplacement = {
+        1: {
+          'regex': /(１日)/g,
+          'replacement': '<ruby>１日<rt>ついたち</rt></ruby>',
+        },
+        2: {
+          'regex': /(２日)/g,
+          'replacement': '<ruby>２日<rt>ふつか</rt></ruby>',
+        },
+        3: {
+          'regex': /(３日)/g,
+          'replacement': '<ruby>３日<rt>みっか</rt></ruby>',
+        },
+        4: {
+          'regex': /(４日)/g,
+          'replacement': '<ruby>４日<rt>よっか</rt></ruby>',
+        },
+        5: {
+          'regex': /(５日)/g,
+          'replacement': '<ruby>５日<rt>いつか</rt></ruby>',
+        },
+        6: {
+          'regex': /(６日)/g,
+          'replacement': '<ruby>６日<rt>むいか</rt></ruby>',
+        },
+        7: {
+          'regex': /(７日)/g,
+          'replacement': '<ruby>７日<rt>なのか</rt></ruby>',
+        },
+        8: {
+          'regex': /(８日)/g,
+          'replacement': '<ruby>８日<rt>ようか</rt></ruby>',
+        },
+        9: {
+          'regex': /(９日)/g,
+          'replacement': '<ruby>９日<rt>ここのか</rt></ruby>',
+        },
+        10: {
+          'regex': /(１０日)/g,
+          'replacement': '<ruby>１０日<rt>とうか</rt></ruby>',
+        },
+        14: {
+          'regex': /(１４日)/g,
+          'replacement': '<ruby>１４日<rt>じゅうよっか</rt></ruby>',
+        },
+        20: {
+          'regex': /(２０日)/g,
+          'replacement': '<ruby>２０日<rt>はつか</rt></ruby>',
+        },
+        24: {
+          'regex': /(２４日)/g,
+          'replacement': '<ruby>２４日<rt>にじゅうよっか</rt></ruby>',
+        },
+      }
+      const dateReplacementDefault = {
+        'regex': /(日)/g,
+        'replacement': '<ruby>日<rt>にち</rt></ruby>',
+      }
+
+      const replacementValues = dateReplacement[date.value.getDate()] || dateReplacementDefault;
+
+       return `${date.value.getDate()}日`
+        .replace(/[0-9]{1}/g, shiftCharacter(0xFEE0))
+        .replace(replacementValues.regex, replacementValues.replacement);
+    };
 
     return {
       date,
@@ -183,6 +256,8 @@ export default {
       daysDate,
       Day,
       Subject,
+      DateString,
+      QuestionString,
       nextDayClass,
       prevDayClass,
       prevAndNextKeys,
